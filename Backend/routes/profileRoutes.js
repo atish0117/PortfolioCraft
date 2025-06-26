5b0729f76f4a8d9ba964d537504100ba86018ef1
atish0117 <atishr334@gmail.com>
2025-06-26 16:46:51 +0530

    update profileRoutes

--- Backend/routes/profileRoutes.js
+++ Backend/routes/profileRoutes.js
@@ -1,5 +1,7 @@
 import express from "express";
 import { updateProfile,updateSectionSettings  } from "../controllers/profileController.js";
+import { addTestimonial, deleteTestimonial } from "../controllers/profileController.js";
+
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
@@ -7,5 +9,7 @@ const router = express.Router();
 router.put("/", protect, updateProfile);
 
 router.put("/section-settings", protect, updateSectionSettings);
+router.post("/testimonial", protect, addTestimonial);
+router.delete("/testimonial/:index", protect, deleteTestimonial);
 
 export default router;
